be39f1a5cf979e689178d7efe17da2ab1e7256b0
Alicia <aliciajimenez@Alicias-MBP.lan>
2022-07-03 23:49:23 -0400

    updated README.md, added index, started css/js

---
+++ assets/js/questions.js
@@ -0,0 +1,91 @@
+// questions, options, and answers
+let questions = [
+  {
+    question: "Where is the correct place to inset a Javascript?",
+    answers: [
+      {option: "The <footer> sections", answer: false},
+      {option: "Both the <head> section and the <body> section", answer: true},
+      {option: "The <body> section", answer: false},
+      {option: "The <head> section", answer: false},
+    ]
+    },
+    {
+    question: "The external Javascript file must contain <script> tag?",
+    answers: [
+      {option: "True", answer: false},
+      {option: "False", answer: true},
+    ]
+    },
+    {
+    question: "Inside which HTML element do we put the Javascript?",
+    answers: [
+      {option: "<javascript>", answer: false},
+      {option: "<script>", answer: true},
+      {option: "<js>", answer: false},
+      {option: "<jscript>", answer: false},
+
+    ]
+    },
+    {
+    question: "How do you insert a comment that has more than one line?",
+    answers: [
+      {option: "//This comment has more than one line//", answer: false},
+      {option: "<!--This comment has more than one line-->", answer: false},
+      {option: "/*This comment has more than one line*/", answer: true},
+      {option: "/`/This comment has more than one line/`/", answer: false},
+    ]
+    },
+    {
+      question: "How to write an IF statement for executing a code if 'i' is NOT equal to 5?",
+      answers: [
+        {option: "if (i!=5)", answer: true},
+        {option: "if i=! 5 then", answer: false},
+        {option: "if i <> 5", answer: false},
+        {option: "if (i <> 5)", answer: false},
+    ]
+    },
+    { 
+      question: "How do you write 'Hello World' in an alert box?",
+      answers: [
+      {option: "alertBox('Hello World');", answer: false},
+      {option: "alert('Hello World');", answer: true},
+      {option: "msgBox('Hello World');", answer: false},
+      {option: "msg('Hello World');", answer: false},
+    ]
+    },
+    {
+    question: "Javascript is the same as Java?",
+    answers: [
+      {option: "False", answer: true},
+      {option: "True", answer: false},
+    ]
+    },
+    { 
+      question: "Which operator is used to assign a value to a variable?",
+      answers: [
+        {option: "X", answer: false},
+        {option: "*", answer: false},
+        {option: "=", answer: true},
+        {option: "--", answer: false},
+    ]
+    },
+    {
+      question: "How do you create a function in Javascript?",
+      answers: [
+        {option: "function = myFunction()", answer: false},
+        {option: "funtion:myFunction()", answer: false},
+        {option: "function= .myFunction()", answer: false},
+        {option: "function myFunction()", answer: true},
+    ]
+    },
+    {
+      question: "How do you call a function named myFunction?",
+      answers: [
+        {option: "call myFunction()", answer: false},
+        {option: "call funtion myFunction()", answer: false},
+        {option: "myFuntion()", answer: true},
+        {option: "call = myFuntion()", answer: false},
+    ]
+    },
+  ]
+      
