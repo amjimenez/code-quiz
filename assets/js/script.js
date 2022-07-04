e3dcface50368046e97a5e9aaef7e1cefe47e370
Alicia <aliciajimenez@Alicias-MBP.lan>
2022-07-04 12:57:17 -0400

    updated README.md, added css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 //variables
-var startButton = document.querySelector('#startBtn');
+var startBtn = document.querySelector('#startBtn');
 var timeElement = document.querySelector("#time");
 var infoContainer = document.querySelector('.info-box');
+var exitBtn = infoBox.querySelector(".button .quit");
+var beginBtn = infoBox.querySelector(".button .restart");
 var quizContainer = document.querySelector('.quiz-box');
+var optionList = document.querySelector(".optionList");
 var resultsContainer = document.querySelector('#results')
 var option_list = document.querySelector(".optionList");
 
@@ -11,3 +14,19 @@ var currentQuestionIndex = 0;
 var timeLeft = 75;
 var timeInterval;
 var score = 0;
+
+startBtn.onclick = ()=>{
+    infoBox.classList.add("activeInfo");
+}
+
+exitBtn.onclick = ()=>{
+    infoBox.classList.remove("activeInfo");
+}
+
+beginBtn.onclick = ()=>{
+    infoBox.classList.remove("activeInfo");
+    quizContainer.classList.add("activeQuiz");
+    showQuestions (0);
+    queCounter (1);
+    startTimer (75);
+}
